c4aa308711f4a091b10b6018bba1dfcf5a94eca9
Jason Conklin <j@ninly.net>
2018-06-01 05:41:49 -0500

    Check wolfram number input

--- genEcaRule.m
+++ genEcaRule.m
@@ -1,5 +1,9 @@
-function ruleVec = genEcaRule(WolframNumber)
-    Wbin = dec2bin(WolframNumber,8);
+function ruleVec = genEcaRule(wolframNumber)
+    if wolframNumber > 255
+        error('genEcaRule:ValueError',...
+            'Wolfram number must be in the range 0-255')
+    end
+    Wbin = dec2bin(wolframNumber,8);
     Wbin = reverse(Wbin)';
     ruleVec = str2num(Wbin);
 end
